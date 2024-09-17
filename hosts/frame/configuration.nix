9e952672e1b2696c976f505df30fc53eefd1229b
Vo Minh Thu <thu@hypered.io>
2024-09-17 23:33:24 +0200

    Disable nix-channel.

--- hosts/frame/configuration.nix
+++ hosts/frame/configuration.nix
@@ -133,6 +133,9 @@
     powerline-fonts
   ];
 
+  nix.nixPath = [ "nixpkgs=${pkgs.path}" ];
+  nix.channel.enable = false;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
