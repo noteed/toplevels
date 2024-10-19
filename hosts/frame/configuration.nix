27a2f2ae78a11c76d405d15ec4eafcdea3b00625
Vo Minh Thu <thu@hypered.io>
2024-10-19 21:43:27 +0200

    Use the GPG agent.

--- hosts/frame/configuration.nix
+++ hosts/frame/configuration.nix
@@ -118,6 +118,8 @@
     vim
   ];
 
+  programs.gnupg.agent.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
